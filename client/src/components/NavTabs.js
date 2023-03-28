671e6ee42e838bd4fb25c75de76ea65e8fa5ccdc
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-03-28 14:09:27 -0400

    added nav tabs: about us, toys, treats and cart

---
+++ client/src/components/NavTabs.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export default function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+        id="navs"
+          href="#AboutUs"
+          onClick={() => handlePageChange('AboutUs')}
+          className= {currentPage === 'AboutUs' ? 'nav-link active' : 'nav-link'}
+        >
+          About Us
+        </a>
+      </li>
+      <li className="nav-item">
+        <a id="navs"
+          href="#Toys"
+          onClick={() => handlePageChange('Toys')}
+          className={currentPage === 'Toys' ? 'nav-link active' : 'nav-link'}
+        >
+          Toys
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+        id="navs"
+          href="#Treats"
+          onClick={() => handlePageChange('Treats')}
+          className={currentPage === 'Treats' ? 'nav-link active' : 'nav-link'}
+        >
+          Treats
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+        id="navs"
+          href="#Cart"
+          onClick={() => handlePageChange('Cart')}
+          className={currentPage === 'Cart' ? 'nav-link active' : 'nav-link'}
+        >
+          Cart
+        </a>
+      </li>
+    </ul>
+  );
+}
