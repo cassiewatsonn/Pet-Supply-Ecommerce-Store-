0ab2891507518051098e24178090ce56d5c14c3b
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-03-28 13:28:52 -0400

    added basic header including the business name. Will add nav tabs later

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header(props) {
+    return (
+        <header>
+            <div className="businessName">
+                <h1>Pet Supply</h1>
+            </div>
+                 {/* {props.children} */}
+        </header>
+   )
+}
