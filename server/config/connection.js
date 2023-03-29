d0939fb07b6e7e5724cc47725fb9c3d8231c152f
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-03-29 11:24:13 -0400

    added client side utils-auth.js JWT token

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/petTreatsDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
