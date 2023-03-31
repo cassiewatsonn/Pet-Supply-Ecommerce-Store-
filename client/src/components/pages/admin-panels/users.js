f425be2962401906630e388a94ac1692e212f529
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-03-31 17:10:28 -0400

    Worked on the admin - users panel.  Removed the loop, fixed some react coding, changed variables & definitions, added function to the .map to remove the loop and fixed the React Form

--- client/src/components/pages/admin-panels/users.js
+++ client/src/components/pages/admin-panels/users.js
@@ -1,41 +1,46 @@
-import React from 'react'; 
-import { Nav, Row, Col, ListGroup, Form} from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Nav, Row, Col, ListGroup, Form } from 'react-bootstrap';
 import { QUERY_USERS, SINGLE_USER } from '../../../utils/queries';
 import { useQuery, useLazyQuery } from '@apollo/client';
 
 export default function UsersAdmin() {
-    const { data } = useQuery(QUERY_USERS);
-    const [ getUser , { loading, error, userData }] = useLazyQuery(SINGLE_USER);
-    const users = data?.users || [];
-    function handleUserData(e) {
-        console.log(e)
-        // {() => getUser({variables: {userId: user._id}})}
+    const [userData, setUserData] = useState();
+    const { data: usersQuery } = useQuery(QUERY_USERS);
+    const [getUser] = useLazyQuery(SINGLE_USER);
+    const users = usersQuery?.users || [];
+
+    async function handleUserData(e) {
+        const { called, data } = await getUser({ variables: { userId: e } })
+        if (called && data) {
+            setUserData(data.user)
+        }
     }
-    return(
+    return (
         <>
             <ListGroup>
-                {users.map((user) => (<ListGroup.Item key={user._id} action onClick={handleUserData(user._id)}>{user.firstName} {user.lastName}</ListGroup.Item>))}
+                {users.map((user) => (<ListGroup.Item key={user._id} action onClick={() => handleUserData(user._id)}>{user.firstName} {user.lastName}</ListGroup.Item>))}
             </ListGroup>
+
             <EditBox userData={userData} />
         </>
-        )
-    }
+    )
+}
 
-function EditBox(userData) {
+function EditBox({ userData }) {
     
-    console.log(userData);
-    return (<Form>
+
+    return userData ? (<Form>
         <Form.Group className="mb-3" controlId="editUser.ControlEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com">{userData.email}</Form.Control>
+            <Form.Control type="email" placeholder="name@example.com" value={userData.email}/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="editUser.ControlName">
             <Form.Label>First Name</Form.Label>
-            <Form.Control type="input"></Form.Control>
+            <Form.Control value={userData.firstName}/>
             <Form.Label>Last Name</Form.Label>
-            <Form.Control type="input"></Form.Control>
+            <Form.Control value={userData.lastName}/>
         </Form.Group>
     </Form>
-    )
+    ) : (<></>)
 
-    }
+}
