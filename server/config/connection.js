e4e7e806c7013e1cf71e3c7a9d54f1afee6dd7dc
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-03-31 14:40:59 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/petTreatsDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  
 });
 
 module.exports = mongoose.connection;
