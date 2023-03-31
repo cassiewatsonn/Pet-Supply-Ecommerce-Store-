ab4d55e338ac3d9824707f4b5a5f2e90fb4e9f2c
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-03-31 11:28:30 -0400

    pushing up my cart changes as the last pull request doesn't seem to have successfully pushed

--- client/src/components/pages/Store.js
+++ client/src/components/pages/Store.js
@@ -8,17 +8,17 @@ export default function Store() {
     console.log(productsArray);
     return (
         <>
-                    <Container>
-            <h1 align="center" className='p-3'>Welcome to the store</h1>
-            {/* <CartComponent></CartComponent> */}
-            <Row xs={1} md={3} className='g-4'>
-                {productsArray.map((product, index) => (
-                    <Col align="center" key={index}>
-                        <ProductCard product={product}/>
-                    </Col>
-                ))}
+            <Container>
+                <h1 align="center" className='p-3'>Welcome to the store</h1>
+                {/* <CartComponent></CartComponent> */}
+                <Row xs={1} md={3} className='g-4'>
+                    {productsArray.map((product, index) => (
+                        <Col align="center" key={index}>
+                            <ProductCard product={product} />
+                        </Col>
+                    ))}
 
-            </Row>
+                </Row>
             </Container>
         </>
     )
