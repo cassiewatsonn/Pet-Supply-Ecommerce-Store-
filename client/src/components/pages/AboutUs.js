d39459273694673ebdabb33f040a47c578e36b78
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-04-02 17:47:44 -0400

    fixed header background colour styling and changes about description

--- client/src/components/pages/AboutUs.js
+++ client/src/components/pages/AboutUs.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function AboutUs (){ 
     return (
-        <div>
+        <div className='about-us'>
             <h2 className='about-title'>About Our Company</h2>
 
             <div className='about-info'> 
@@ -12,15 +12,15 @@ export default function AboutUs (){
                     <img src={process.env.PUBLIC_URL+'/images/theo.png'} alt='Theo the Golden Retriever with a Happy Birthday Bone Biscuit' width="40%" height="100%"/>
                 </div>
 
-                <p>Welcome to our dog supply small business! We are a team of dedicated animal lovers who believe that our furry friends deserve only the best. As passionate dog owners ourselves, we understand the importance of providing high-quality toys and treats that not only promote healthy living, but also bring joy to our pets' lives.</p>
+                <p>Welcome to The Pupscale Barkery, your destination for the most delectable dog treats! Our team of passionate animal lovers understands the importance of providing your furry friends with only the best. As dog owners ourselves, we believe in promoting healthy living and bringing joy to our pets' lives through our treats.</p>
 
-                <p>At our business, we specialize in offering a wide range of products, from chew toys and interactive puzzles to nutritious treats made from only the finest ingredients. We know that every dog has their own unique needs, which is why we offer a diverse selection of products to cater to all breeds, sizes, and lifestyles.</p>
+                <p>At The Pupscale Barkery, we specialize in a wide range of dog treats made from only the finest and natural ingredients. We understand that every dog is unique, which is why we offer a diverse selection of treats to cater to all breeds, sizes, and dietary needs.</p>
 
-                <p>Our team is committed to ensuring that all of our products are of the highest quality, so you can feel confident that you are giving your pet the very best. We believe in using only natural, wholesome ingredients in our treats, and we carefully source all of our products to ensure they meet our rigorous standards.</p>
+                <p>Our treats are carefully crafted to ensure that they are not only delicious but also nutritious. We source all of our ingredients from reputable suppliers to guarantee the quality of our products.</p>
 
-                <p>Most importantly, we treat all of our pets like family. We know that they are more than just animals - they are beloved members of our households. That's why we strive to create a warm, welcoming environment for all of our customers and their furry companions. We believe that every pet deserves to be treated with the love, respect, and care that they deserve.</p>
+                <p>We treat all of our pets like family, and we believe in creating a warm, welcoming environment for all of our customers and their furry companions. We understand that every pet deserves to be treated with love, respect, and care, which is why we go above and beyond to provide exceptional service and products.</p>
 
-                <p>So whether you're looking for the perfect toy to keep your dog entertained, or a delicious treat to reward them for a job well done, we've got you covered. Thank you for choosing us as your go-to source for all of your dog supply needs</p>
+                <p>So if you're looking for the most delicious and nutritious dog treats, The Pupscale Barkery has got you covered. Thank you for choosing us as your go-to source for all of your dog treat needs.</p>
 
             </div>
         </div>
