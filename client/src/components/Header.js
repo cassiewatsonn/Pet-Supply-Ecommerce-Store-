7b40ace1a103bf958e576bbe9881fcd2e0c52a4a
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-04-03 10:26:08 -0400

    added logo to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,11 +4,8 @@ import '../App.js'
 export default function Header(props) {
     return (
         <header className='header'>
-            {/* <img src='images/logo-mossgreen-300x1820-black-text.png' alt='The Pupscale Bakery Logo'/> */}
             <img src='images/logo-xsmall.png' alt='The Pupscale Bakery Logo' />
-
             <div className="businessName">
-
                 <h1>The Pupscale Barkery</h1>
 
             </div>
